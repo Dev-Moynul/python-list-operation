4260cf709d63b843fd2130987fbc10775c3c5f4b
Dev-Moynul <stu.moynulislam@gmail.com>
2026-04-17 12:32:11 +0600

    string comperehension

--- list_comperehension.py
+++ list_comperehension.py
@@ -2,3 +2,7 @@ squares=[x**2 for x in range(10)]
 print(squares)
 cube=[y**3 for y in range(50) if y%2==0]
 print(cube)
+data='I love python and also data Science'
+data=data.split()
+[x for x in data]
+print(data)
